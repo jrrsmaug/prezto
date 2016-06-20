e1c6b14d39bade94494d7139e3262db0d2050c42
Bernhard Sillipp <bernhard.sillipp@novum-ccservices.at>
2016-06-20 15:50:13 +0200

    load up multiple ssh ids

--- modules/ssh/init.zsh
+++ modules/ssh/init.zsh
@@ -4,12 +4,12 @@
 # Authors:
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
 #
-
+     
 # Return if requirements are not found.
 if [[ "$OSTYPE" == darwin* ]] || (( ! $+commands[ssh-agent] )); then
   return 1
 fi
-
+     
 # Set the path to the SSH directory.
 _ssh_dir="$HOME/.ssh"
 
@@ -18,14 +18,14 @@ _ssh_agent_env="${_ssh_agent_env:-${TMPDIR:-/tmp}/ssh-agent.env}"
 
 # Set the path to the persistent authentication socket.
 _ssh_agent_sock="${TMPDIR:-/tmp}/ssh-agent.sock"
-
+     
 # Start ssh-agent if not started.
 if [[ ! -S "$SSH_AUTH_SOCK" ]]; then
   # Export environment variables.
   source "$_ssh_agent_env" 2> /dev/null
-
+     
   # Start ssh-agent if not started.
-  if ! ps -U "$LOGNAME" -o pid,ucomm | grep -q -- "${SSH_AGENT_PID:--1} ssh-agent"; then
+  if ! ps | grep "ssh-agent"; then
     eval "$(ssh-agent | sed '/^echo /d' | tee "$_ssh_agent_env")"
   fi
 fi
